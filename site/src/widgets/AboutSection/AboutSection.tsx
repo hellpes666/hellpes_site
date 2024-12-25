d8e1426ac4c56725a808eed72a2114bb16061300
Barmatov Alexey <alexrui_hpow@mail.ru>
2024-12-25 16:09:38 +0300

    watchout

--- site/src/widgets/AboutSection/AboutSection.tsx
+++ site/src/widgets/AboutSection/AboutSection.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
-//TODO Разобраться с отправкой писем и SMTP
+//TODO Разобраться с отправкой писем и SMTP йоу
 
 const ContactForm = () => {
   const [status, setStatus] = useState<
